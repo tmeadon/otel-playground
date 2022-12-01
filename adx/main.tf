b1b331cfab1e2c85ed640f0b85a23a20b7f09a37
Tom Meadon <tom@meadon.me>
2022-12-01 14:41:10 +0000

    add service bus

--- adx/main.tf
+++ adx/main.tf
@@ -16,6 +16,8 @@ resource "azurerm_kusto_cluster" "cluster" {
     name = "Standard_E4ads_v5"
     capacity = 2
   }
+
+  streaming_ingestion_enabled = true
 }
 
 resource "azurerm_kusto_database" "otel" {
@@ -26,4 +28,27 @@ resource "azurerm_kusto_database" "otel" {
 
   hot_cache_period = "P1D"
   soft_delete_period = "P7D"
+}
+
+resource "azurerm_servicebus_namespace" "sb" {
+  name = "oteltest09"
+  location  = "uksouth"
+  resource_group_name = azurerm_resource_group.otel_adx.name
+
+  sku = "Standard"
+}
+
+resource "azurerm_servicebus_queue" "queue1" {
+  name = "queue"
+  namespace_id = azurerm_servicebus_namespace.sb.id
+  enable_partitioning = true 
+}
+
+output "sb_connection" {
+  value = azurerm_servicebus_namespace.sb.default_primary_connection_string
+  sensitive = true
+}
+
+output "sb_queue_name" {
+  value = azurerm_servicebus_queue.queue1.name
 }
